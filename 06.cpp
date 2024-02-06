80335280dd919af67acfce2c4b10dd27083eaae6
Ayan Khan <ayanxkhan12@gmail.com>
2024-02-06 16:36:28 +0530

    Day06

---
+++ 06.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int arr[5];
+    cout << "Enter the elements of array:" << endl;
+    for(int i=0;i<5;i++){
+        cin >> arr[i];
+    }
+
+
+    cout << "The elements of array are:";
+
+    for(int i=0;i<5;i++){
+        cout <<  " " << arr[i];
+    }
+
+    return 0;
+
+}
+
+
